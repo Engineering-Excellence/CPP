0dfc5177d9872452281ed6b7133badd09b8686e6
Kyle <propheta@yonsei.ac.kr>
2025-06-08 20:19:26 +0900

    HelloWorld로 본 C++ - 첫 번째 C++ 프로그램

---
+++ nullnull/chapter01/HelloCpp/HelloCpp.cpp
@@ -0,0 +1,13 @@
+// Chapter01 C와는 다른 C++
+
+// 1.2 HelloWorld로 본 C++
+
+// HelloCpp.cpp: 콘솔 응용 프로그램에 대한 진입점을 정의
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+
+    return 0;
+}
