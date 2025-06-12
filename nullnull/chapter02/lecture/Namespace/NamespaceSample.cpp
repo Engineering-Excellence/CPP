a8d677549b69dda6ef3e561c5ef84492587bcf10
Kyle <propheta@yonsei.ac.kr>
2025-06-12 20:04:30 +0900

    네임스페이스 - 네임스페이스

---
+++ nullnull/chapter02/lecture/Namespace/NamespaceSample.cpp
@@ -0,0 +1,22 @@
+// Chapter02 C++ 함수와 네임스페이스
+
+// 2.4 네임스페이스
+
+// 2.4.1 네임스페이스
+
+#include <iostream>
+
+namespace test {
+    int g_nData = 100;
+
+    void testFunc() {
+        std::cout << "test::testFunc()" << std::endl;
+    }
+}
+
+int main() {
+    test::testFunc();
+    std::cout << test::g_nData << std::endl;
+
+    return 0;
+}
